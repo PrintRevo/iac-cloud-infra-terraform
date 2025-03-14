e2bb532db792664279cdc658ab6426d068810500
Jamiu Igbalajobi <kreatifcode@gmail.com>
2025-03-14 04:22:14 +0100

    update name

--- bin/terraform-apply.sh
+++ bin/terraform-apply.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 set -e  # Exit immediately if a command exits with a non-zero status
 
-echo "Initializing Terraform..."
-terraform init 
-
-echo "Validating Terraform configuration..."
-terraform validate
-
-
-echo "Creating execution plan..."
-terraform plan -out=tfplan
 
 # Apply the Terraform configuration
 # terraform apply -auto-approve
