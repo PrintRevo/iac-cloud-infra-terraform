e935f0aa01304f596270b0c4a3cc1fe16e20c46c
Jamiu Igbalajobi <kreatifcode@gmail.com>
2025-03-14 06:47:32 +0100

    fixing terraform cicd

--- aws/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "printrevo-terraform-state-bucket"
-    key            = "terraform.tfstate"
-    region         = "eu-central-1"
-    encrypt        = true
-    dynamodb_table = "printrevo-terraform-lock-table"
-  }
-}
--- main.tf
+++ main.tf
@@ -9,6 +9,13 @@ terraform {
     #   version = "~> 2.0"
     # }
   }
+  backend "s3" {
+    bucket         = "printrevo-terraform-state-bucket"
+    key            = "terraform.tfstate"
+    region         = "eu-central-1"
+    encrypt        = true
+    dynamodb_table = "printrevo-terraform-lock-table"
+  }
 }
 
 
