9798b4ae24701faf70d16083910456b2e92498b1
Jamiu Igbalajobi <kreatifcode@gmail.com>
2025-03-21 21:08:52 +0100

    uncomment all resources needed

--- main.tf
+++ main.tf
@@ -20,43 +20,43 @@ terraform {
   }
 }
 
-# module "main_vpc" {
-#   source      = "./modules/aws/vpc"
-#   environment = var.environment
-#   aws_region  = var.aws_profile
-# }
+module "main_vpc" {
+  source      = "./modules/aws/vpc"
+  environment = var.environment
+  aws_region  = var.aws_profile
+}
 
-# module "ecr_repositories" {
-#   source      = "./modules/aws/ecr"
-#   environment = var.environment
-# }
+module "ecr_repositories" {
+  source      = "./modules/aws/ecr"
+  environment = var.environment
+}
 
-# module "ecs_cluster_and_services" {
-#   source      = "./modules/aws/ecs"
-#   environment = var.environment
-# }
+module "ecs_cluster_and_services" {
+  source      = "./modules/aws/ecs"
+  environment = var.environment
+}
 
-# module "iam_role_and_permission" {
-#   source      = "./modules/aws/iam"
-#   environment = var.environment
-# }
+module "iam_role_and_permission" {
+  source      = "./modules/aws/iam"
+  environment = var.environment
+}
 
-# module "storage_services" {
-#   source     = "./modules/aws/storages"
-#   depends_on = [module.main_vpc]
-#   subnet_ids = [
-#     module.main_vpc.public_subnet_a_id,
-#     module.main_vpc.public_subnet_b_id
-#   ]
-#   environment                         = var.environment
-#   aws_profile                         = var.aws_profile
-#   rds_username                        = var.rds_username
-#   rds_password                        = var.rds_password
-#   aws_security_group_public_access_id = module.main_vpc.aws_security_group_public_access_id
-#   redis_node_type                     = var.redis_node_type
-#   rds_instance_class                  = var.rds_instance_class
-#   aws_region                          = var.aws_region
-# }
+module "storage_services" {
+  source     = "./modules/aws/storages"
+  depends_on = [module.main_vpc]
+  subnet_ids = [
+    module.main_vpc.public_subnet_a_id,
+    module.main_vpc.public_subnet_b_id
+  ]
+  environment                         = var.environment
+  aws_profile                         = var.aws_profile
+  rds_username                        = var.rds_username
+  rds_password                        = var.rds_password
+  aws_security_group_public_access_id = module.main_vpc.aws_security_group_public_access_id
+  redis_node_type                     = var.redis_node_type
+  rds_instance_class                  = var.rds_instance_class
+  aws_region                          = var.aws_region
+}
 
 module "github_repositories" {
   source = "./modules/github"
