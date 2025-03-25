3c935bace5fb35e9f8ec9926489441848d8ef3d9
Jamiu Igbalajobi <kreatifcode@gmail.com>
2025-03-25 01:29:53 +0100

    wip

--- bin/terraform-import.sh
+++ bin/terraform-import.sh
@@ -80,7 +80,7 @@ for ARN in $RESOURCE_ARNS; do
     ;;
   esac
 
-  if terraform state list | grep "$TF_RESOURCE.$RESOURCE_ID"; then
+  if terraform state list | grep -q "$TF_RESOURCE.$RESOURCE_ID"; then
     echo "Resource $TF_RESOURCE.$RESOURCE_ID already managed by Terraform. Skipping import."
     continue
   fi
@@ -90,9 +90,17 @@ for ARN in $RESOURCE_ARNS; do
     echo "Successfully imported $TF_RESOURCE.$RESOURCE_ID"
   else
     echo "Error importing $TF_RESOURCE.$RESOURCE_ID. Skipping..."
+    continue
   fi
 
-   # Read JSON files in ./datas/repository-definitions
+  # Read JSON files in ./datas/repository-definitions
+  for FILE in ./datas/repository-definitions/*.json; do
+    NAME=$(jq -r '.repo_name' "$FILE")
+    if terraform state list | grep -q "$NAME"; then
+      echo "Resource $NAME already managed by Terraform. Skipping import."
+      continue 2
+    fi
+  done
   # for FILE in ./datas/repository-definitions/*.json; do
   #   NAME=$(jq -r '.name' "$FILE")
   #   if terraform state list | grep "$NAME"; then
