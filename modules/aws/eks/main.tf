7edd37a67634b515ee14db3b69839e425f40c981
Jamiu Igbalajobi <kreatifcode@gmail.com>
2025-03-31 12:17:27 +0100

    wip

--- modules/aws/eks/main.tf
+++ modules/aws/eks/main.tf
@@ -82,7 +82,12 @@ resource "aws_eks_node_group" "eks_cluster_node_group" {
     min_size     = var.min_size
   }
 
-  depends_on = [aws_eks_cluster.eks_cluster]
+  depends_on = [
+    aws_eks_cluster.eks_cluster,
+    aws_iam_role_policy_attachment.example-AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.example-AmazonEKS_CNI_Policy,
+    aws_iam_role_policy_attachment.example-AmazonEC2ContainerRegistryReadOnly
+  ]
   tags = {
     Environment = var.environment
     Schedule    = "EKSInstanceSchedule"
